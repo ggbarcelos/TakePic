f9f967b99e1bd9d8cc19b28ac646f0e33eb16bdc
Glauber Gomes Barcelos <glauberbarcelos@hotmail.com>
2017-09-16 19:29:40 -0300

    Second Commit

--- TakePic/TakePic/MainPage.xaml.cs
+++ TakePic/TakePic/MainPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Media;
 
 namespace TakePic
 {
@@ -12,6 +9,105 @@ namespace TakePic
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void TirarFoto(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma câmera detectada.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo"
+                });
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void EscolherFoto(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de fotos não suportada.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void GravarVideo(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsTakeVideoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma câmera detectada.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakeVideoAsync(
+
+                new Plugin.Media.Abstractions.StoreVideoOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo",
+                    Quality = Plugin.Media.Abstractions.VideoQuality.Medium
+                });
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void EscolherVideo(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickVideoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de videos não suportada.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.PickVideoAsync();
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
         }
     }
 }
